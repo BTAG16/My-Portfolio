b1f76dec8239bf51b2322ac67e09d4e1d04e154b
BTAG16 <rumeighoraye@gmail.com>
2025-03-30 13:55:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -41,4 +41,5 @@ def terms_of_service():
     return render_template('terms_of_service.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
